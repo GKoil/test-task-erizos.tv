9177203a24f15ef7f5e94d2749afbc24b9e83e58
Kirill <kirill@Kirills-MacBook-Pro.local>
2024-06-17 23:22:39 +0400

    fix build app

--- task-3-react-app/vite.config.ts
+++ task-3-react-app/vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@myTypes': path.resolve(__dirname, 'src/types'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@mock': path.resolve(__dirname, 'src/mock'),
+      '@constants': path.resolve(__dirname, 'src/constants.ts'),
+    },
+  },
 });
